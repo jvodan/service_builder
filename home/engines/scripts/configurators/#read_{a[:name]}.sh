5d352aa5900538713bc50013cae3fde672e7cb77
James L Vodanovich <james@engines.onl>
2020-05-06 08:54:12 +1000

    Delete #read_{a[:name]}.sh

--- home/engines/scripts/configurators/#read_{a[:name]}.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-if test -f /home/engines/scripts/configurators/saved/db_master_pass
- then
-  cat /home/engines/scripts/configurators/saved/db_master_pass
-else
-  echo '{"db_master_pass":"Not Saved"}'
-fi
