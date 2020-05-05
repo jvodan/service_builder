1866d718a94e025676edf94384328909a1f41769
James Vodanovich <james@jvodan.com>
2020-05-06 08:47:44 +1000

    ignore

---
+++ home/engines/scripts/configurators/#set_{a[:name]}.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+
+ . /home/engines/functions/checks.sh
+ required_values="db_master_pass"
+check_required_values
+ 
+if test -n $db_master_pass
+ then  
+  mysql -urma -e "set password for 'root'@'%' = PASSWORD('$db_master_pass'); "	 			
+fi
+ 
